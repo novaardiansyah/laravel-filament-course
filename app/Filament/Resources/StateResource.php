5e5090fadf8fe438dc14e5503f2289746503b0b0
novaardiansyah <novaardiansyah78@gmail.com>
2024-02-01 10:14:28 +0700

    Customixe dashboard and sidebar panel

--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -17,7 +17,7 @@ class StateResource extends Resource
 {
   protected static ?string $model = State::class;
 
-  protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+  protected static ?string $navigationIcon = 'heroicon-o-building-library';
   protected static ?string $navigationGroup = 'System Manangement';
   protected static ?string $modelLabel = 'States';
   protected static ?string $navigationLabel = 'State';
