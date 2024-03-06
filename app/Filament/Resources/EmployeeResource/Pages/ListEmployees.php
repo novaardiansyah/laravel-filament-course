0d961bebbf21c8be9789676b94542dfa57d18578
Nova  Ardiansyah <novaardiansyah78@gmail.com>
2024-03-06 14:20:25 +0700

    Tab

--- app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -3,17 +3,36 @@
 namespace App\Filament\Resources\EmployeeResource\Pages;
 
 use App\Filament\Resources\EmployeeResource;
+use App\Models\Employee;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListEmployees extends ListRecords
 {
-    protected static string $resource = EmployeeResource::class;
+  protected static string $resource = EmployeeResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
+  protected function getHeaderActions(): array
+  {
+    return [
+      Actions\CreateAction::make(),
+    ];
+  }
+
+  public function getTabs(): array 
+  {
+    return [
+      'All' => Tab::make(),
+      'This Week' => Tab::make()
+        ->modifyQueryUsing(fn (Builder $query) => $query->where('date_of_hired', '>=', now()->subWeek()))
+        ->badge(Employee::query()->where('date_of_hired', ' >= ', now()->subWeek())->count()),
+      'This Month' => Tab::make()
+        ->modifyQueryUsing(fn (Builder $query) => $query->where('date_of_hired', '>=', now()->subMonth()))
+        ->badge(Employee::query()->where('date_of_hired', ' >= ', now()->subMonth())->count()),
+      'This Year' => Tab::make()
+        ->modifyQueryUsing(fn (Builder $query) => $query->where('date_of_hired', '>=', now()->subYear()))
+        ->badge(Employee::query()->where('date_of_hired', ' >= ', now()->subYear())->count()),
+    ];
+  }
 }
